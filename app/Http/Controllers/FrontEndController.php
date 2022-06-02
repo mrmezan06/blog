33465d477f9df71b476d3fedb2e6fa5f22524818
Mejanur Rahman Akash <mrmezan06@gmail.com>
2022-06-03 02:32:23 +0600

    Dynamic content Display III

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -16,7 +16,10 @@ class FrontEndController extends Controller
         ->with('categories', Category::take(5)->get())
         ->with('first_post', Post::orderBy('created_at', 'desc')->first())
         ->with('second_post', Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
-        ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first());
+        ->with('third_post', Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
+        ->with('laravel', Category::find(2))
+        ->with('wordpress', Category::find(3))
+        ->with('settings', Setting::first());
         
     }
 }
